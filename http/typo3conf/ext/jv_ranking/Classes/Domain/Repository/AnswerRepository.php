4740d2b6d9ec1638d98cac59b59aa92cb6a5b493
Joerg Velletti <jvelletti@gmail.com>
2019-08-11 07:05:46 +0200

    [TANGO] Ranking Module ony works for ONE organizer .. fixed

--- http/typo3conf/ext/jv_ranking/Classes/Domain/Repository/AnswerRepository.php
+++ http/typo3conf/ext/jv_ranking/Classes/Domain/Repository/AnswerRepository.php
@@ -39,4 +39,29 @@ class AnswerRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
         // die;
         return $res ;
     }
+
+    public function getAllAnswersByOrganizerUid(  $organizerUid )
+    {
+        $query = $this->createQuery();
+        $querySettings = $query->getQuerySettings() ;
+        $querySettings->setRespectStoragePage(false);
+        $querySettings->setRespectSysLanguage(FALSE);
+        $query->setQuerySettings($querySettings) ;
+
+        $constraints[] = $query->equals('organizerUid', $organizerUid ) ;
+        if( count($constraints) > 1 ) {
+            $query->matching($query->logicalAnd($constraints));
+        } else {
+            $query->matching($constraints);
+        }
+
+        $res = $query->execute() ;
+
+        // new way to debug typo3 db queries
+        // $queryParser = $this->objectManager->get(\TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbQueryParser::class);
+        // var_dump($queryParser->convertQueryToDoctrineQueryBuilder($query)->getSQL());
+        // var_dump($queryParser->convertQueryToDoctrineQueryBuilder($query)->getParameters()) ;
+        // die;
+        return $res ;
+    }
 }
